d9116195bce4e04a3587f819db656565a50e9120
ztNozdormu <w1999wtw3537@sina.com>
2025-07-22 22:50:39 +0800

    heikin_ashi_bar_builder,heikin_ashi_bar_builder_factory init

--- src/bar/builder/factory.rs
+++ src/bar/builder/factory.rs
@@ -1,3 +1,4 @@
 pub mod tick_bar_builder_factory;
 pub mod time_bar_builder_factory;
 pub mod volume_bar_builder_factory;
+pub mod heikin_ashi_bar_builder_factory;
--- src/bar/builder/mod.rs
+++ src/bar/builder/mod.rs
@@ -2,4 +2,5 @@ pub mod factory;
 pub mod tick_bar_builder;
 pub mod time_bar_builder;
 pub mod types;
-mod volume_bar_builder;
+pub mod volume_bar_builder;
+pub mod heikin_ashi_bar_builder;
