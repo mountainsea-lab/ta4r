e35d4dcc461dffe80ecc0f5a92092bf31c73f5f0
ztNozdormu <w1999wtw3537@sina.com>
2025-07-29 21:18:36 +0800

    SmaIndicator impl

--- src/indicators/averages/sma_indicator.rs
+++ src/indicators/averages/sma_indicator.rs
@@ -1,1 +1,142 @@
+use crate::bar::types::BarSeries;
+use crate::indicators::Indicator;
+use crate::indicators::abstract_indicator::BaseIndicator;
+use crate::indicators::cached_indicator::CachedIndicator;
+use crate::indicators::helpers::running_total_indicator::RunningTotalIndicator;
+use crate::indicators::types::{IndicatorCalculator, IndicatorError};
+use crate::num::{NumFactory, TrNum};
+use std::marker::PhantomData;
 
+pub struct SmaCalculator<'a, T, S, I>
+where
+    T: TrNum + Clone + 'static,
+    S: for<'any> BarSeries<'any, T>,
+    I: Indicator<Num = T, Series<'a> = S>,
+{
+    running_total: RunningTotalIndicator<'a, T, S, I>,
+    bar_count: usize,
+    _phantom: PhantomData<(T, S)>,
+}
+
+impl<'a, T, S, I> Clone for SmaCalculator<'a, T, S, I>
+where
+    T: TrNum + Clone + 'static,
+    S: for<'any> BarSeries<'any, T>,
+    I: Indicator<Num = T, Series<'a> = S>,
+{
+    fn clone(&self) -> Self {
+        Self {
+            running_total: self.running_total.clone(),
+            bar_count: self.bar_count,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a, T, S, I> SmaCalculator<'a, T, S, I>
+where
+    T: TrNum + Clone + 'static,
+    S: for<'any> BarSeries<'any, T>,
+    I: Indicator<Num = T, Series<'a> = S>,
+{
+    pub fn new(indicator: &'a I, bar_count: usize) -> Self {
+        Self {
+            running_total: RunningTotalIndicator::new(indicator, bar_count),
+            bar_count,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a, T, S, I> IndicatorCalculator<'a, T, S> for SmaCalculator<'a, T, S, I>
+where
+    T: TrNum + Clone + 'static,
+    S: for<'any> BarSeries<'any, T>,
+    I: Indicator<Num = T, Series<'a> = S>,
+{
+    fn calculate(
+        &self,
+        _base: &BaseIndicator<'a, T, S>,
+        index: usize,
+    ) -> Result<T, IndicatorError> {
+        let sum = self.running_total.get_value(index)?;
+        let real_bar_count = (index + 1).min(self.bar_count);
+        let denom = self
+            .running_total
+            .get_bar_series()
+            .num_factory()
+            .num_of_i64(real_bar_count as i64);
+        sum.divided_by(&denom).map_err(IndicatorError::NumError)
+    }
+}
+
+pub struct SmaIndicator<'a, T, S, I>
+where
+    T: TrNum + Clone + 'static,
+    S: for<'any> BarSeries<'any, T>,
+    I: Indicator<Num = T, Series<'a> = S>,
+{
+    cached: CachedIndicator<'a, T, S, SmaCalculator<'a, T, S, I>>,
+}
+
+impl<'a, T, S, I> Clone for SmaIndicator<'a, T, S, I>
+where
+    T: TrNum + Clone + 'static,
+    S: for<'any> BarSeries<'any, T>,
+    I: Indicator<Num = T, Series<'a> = S>,
+{
+    fn clone(&self) -> Self {
+        Self {
+            cached: self.cached.clone(),
+        }
+    }
+}
+
+impl<'a, T, S, I> SmaIndicator<'a, T, S, I>
+where
+    T: TrNum + Clone + 'static,
+    S: for<'any> BarSeries<'any, T>,
+    I: Indicator<Num = T, Series<'a> = S>,
+{
+    pub fn new(indicator: &'a I, bar_count: usize) -> Self {
+        let calculator = SmaCalculator::new(indicator, bar_count);
+        let cached = CachedIndicator::new_from_indicator(indicator, calculator);
+        Self { cached }
+    }
+}
+
+impl<'a, T, S, I> Indicator for SmaIndicator<'a, T, S, I>
+where
+    T: TrNum + Clone + 'static,
+    S: for<'any> BarSeries<'any, T>,
+    I: Indicator<Num = T, Series<'a> = S>,
+{
+    type Num = T;
+    type Series<'b>
+        = S
+    where
+        Self: 'b;
+
+    fn get_value(&self, index: usize) -> Result<T, IndicatorError> {
+        self.cached.get_cached_value(index)
+    }
+
+    fn get_bar_series(&self) -> &Self::Series<'_> {
+        self.cached.get_bar_series()
+    }
+
+    fn get_count_of_unstable_bars(&self) -> usize {
+        self.cached.calculator().bar_count
+    }
+}
+
+impl<'a, T, S, I> std::fmt::Display for SmaIndicator<'a, T, S, I>
+where
+    T: TrNum + Clone + 'static,
+    S: for<'any> BarSeries<'any, T> + std::fmt::Debug,
+    I: Indicator<Num = T, Series<'a> = S>,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SMA(bar_count={})", self.get_count_of_unstable_bars())
+    }
+}
