9e4bfeb57f20ee1ddad6971974655af1c6868508
ztNozdormu <w1999wtw3537@sina.com>
2025-08-03 14:39:07 +0800

    License

--- src/indicators/recursive_cached_indicator.rs
+++ src/indicators/recursive_cached_indicator.rs
@@ -1,87 +1,33 @@
+/*!
+ * MIT License
+ *
+ * Copyright (c) 2025 Mountainsea
+ * Based on ta4j (c) 2017–2025 Ta4j Organization & respective authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 use crate::bar::types::BarSeries;
 use crate::indicators::Indicator;
 use crate::indicators::abstract_indicator::BaseIndicator;
 use crate::indicators::cached_indicator::CachedIndicator;
 use crate::indicators::types::{IndicatorCalculator, IndicatorError};
 use crate::num::TrNum;
-
-// use crate::bar::types::BarSeries;
-// use crate::indicators::Indicator;
-// use crate::indicators::cached_indicator::CachedIndicator;
-// use crate::indicators::types::IndicatorError;
-// use crate::num::TrNum;
-//
-// const RECURSION_THRESHOLD: usize = 100;
-//
-// /// 递归缓存指标
-// pub struct RecursiveCachedIndicator<'a, T, S, F>
-// where
-//     T: TrNum + 'static,
-//     S: BarSeries<'a, T>,
-//     F: Fn(&CachedIndicator<'a, T, S, F>, usize) -> Result<T, IndicatorError> + Clone,
-// {
-//     inner: CachedIndicator<'a, T, S, F>,
-// }
-//
-// impl<'a, T, S, F> RecursiveCachedIndicator<'a, T, S, F>
-// where
-//     T: TrNum + 'static,
-//     S: BarSeries<'a, T>,
-//     F: Fn(&CachedIndicator<'a, T, S, F>, usize) -> Result<T, IndicatorError> + Clone,
-// {
-//     pub fn new(series: &'a S, calculate_fn: F) -> Self {
-//         Self {
-//             inner: CachedIndicator::new_from_series(series, calculate_fn),
-//         }
-//     }
-//
-//     pub fn new_from_indicator<I>(indicator: &'a I, calculate_fn: F) -> Self
-//     where
-//         I: Indicator<Num = T, Series<'a> = S>,
-//     {
-//         Self {
-//             inner: CachedIndicator::new_from_indicator(indicator, calculate_fn),
-//         }
-//     }
-//
-//     pub fn get_value(&self, index: usize) -> Result<T, IndicatorError> {
-//         let series = self.inner.base.get_bar_series();
-//
-//         if let Some(end_index) = series.get_end_index() {
-//             if index <= end_index {
-//                 let removed_bars_count = series.get_removed_bars_count();
-//
-//                 let highest_result_index = *self.inner.highest_result_index.borrow();
-//
-//                 let highest_result_index_usize = if highest_result_index < 0 {
-//                     0
-//                 } else {
-//                     highest_result_index as usize
-//                 };
-//
-//                 let start_index = std::cmp::max(removed_bars_count, highest_result_index_usize);
-//
-//                 if index > start_index + RECURSION_THRESHOLD {
-//                     for prev_index in start_index..index {
-//                         // 这里调用 inner.get_value(&self, usize) 也是不可变借用，
-//                         // 内部用 RefCell 管理可变性，没问题
-//                         self.inner.get_cached_value(prev_index)?;
-//                     }
-//                 }
-//             }
-//         }
-//
-//         self.inner.get_cached_value(index)
-//     }
-//
-//     pub fn get_bar_series(&self) -> &'a S {
-//         self.inner.base.get_bar_series()
-//     }
-//
-//     pub fn into_inner(self) -> CachedIndicator<'a, T, S, F> {
-//         self.inner
-//     }
-// }
 
 const RECURSION_THRESHOLD: usize = 100;
 
