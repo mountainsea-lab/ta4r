853482d35d8ef2f0c2f58e9b10159be8d4f00b7b
www1 <w1999wtw3537@sina.com>
2025-08-13 17:44:18 +0800

    Rule trait use associated types

--- ta4r/src/rule/base_rule.rs
+++ ta4r/src/rule/base_rule.rs
@@ -1,14 +1,21 @@
+use crate::rule::Rule;
 use log::trace;
 use std::marker::PhantomData;
 
 /// 基础规则
 /// 存放公共工具方法，比如调试日志
-pub struct BaseRule<'a, N, CM, HM, S, TR> {
+pub struct BaseRule<'a, R>
+where
+    R: Rule<'a>,
+{
     class_name: &'static str,
-    _marker: PhantomData<(&'a N, CM, HM, S, TR)>,
+    _marker: PhantomData<&'a R>,
 }
 
-impl<'a, N, CM, HM, S, TR> BaseRule<'a, N, CM, HM, S, TR> {
+impl<'a, R> BaseRule<'a, R>
+where
+    R: Rule<'a>,
+{
     /// 创建基础规则
     pub fn new(class_name: &'static str) -> Self {
         Self {
--- ta4r/src/rule/mod.rs
+++ ta4r/src/rule/mod.rs
@@ -23,6 +23,7 @@
  * SOFTWARE.
  */
 
+mod and_rule;
 mod base_rule;
 
 use crate::TradingRecord;
@@ -33,21 +34,85 @@ use crate::num::TrNum;
 /// 一条交易规则（Trading Rule）
 ///
 /// 用于构建交易策略（Strategy），规则之间可以组合成更复杂的逻辑规则。
-pub trait Rule<'a, N, CM, HM, S, TR>
-where
-    N: TrNum + 'static,
-    CM: CostModel<N> + Clone,
-    HM: CostModel<N> + Clone,
-    S: BarSeries<'a, N> + 'a,
-    TR: TradingRecord<'a, N, CM, HM, S>,
-{
+// pub trait Rule<'a, N, CM, HM, S, TR>
+// where
+//     N: TrNum + 'static,
+//     CM: CostModel<N> + Clone,
+//     HM: CostModel<N> + Clone,
+//     S: BarSeries<'a, N> + 'a,
+//     TR: TradingRecord<'a, N, CM, HM, S>,
+// {
+//     /// 规则在给定索引下是否满足（不依赖交易记录）
+//     fn is_satisfied(&self, index: usize) -> bool {
+//         self.is_satisfied_with_record(index, None)
+//     }
+//
+//     /// 规则在给定索引下是否满足（可选提供交易记录）
+//     fn is_satisfied_with_record(&self, index: usize, trading_record: Option<&TR>) -> bool;
+//     //
+//     // /// 与另一条规则组合成 AND 规则
+//     // fn and<R>(self, other: R) -> AndRule<'a, N, CM, HM, S, TR, Self, R>
+//     // where
+//     //     Self: Sized,
+//     //     R: Rule<'a, N, CM, HM, S, TR>,
+//     // {
+//     //     AndRule::new(self, other)
+//     // }
+//     //
+//     // /// 与另一条规则组合成 OR 规则
+//     // fn or<R>(self, other: R) -> OrRule<'a, N, CM, HM, S, TR, Self, R>
+//     // where
+//     //     Self: Sized,
+//     //     R: Rule<'a, N, CM, HM, S, TR>,
+//     // {
+//     //     OrRule::new(self, other)
+//     // }
+//     //
+//     // /// 与另一条规则组合成 XOR 规则
+//     // fn xor<R>(self, other: R) -> XorRule<'a, N, CM, HM, S, TR, Self, R>
+//     // where
+//     //     Self: Sized,
+//     //     R: Rule<'a, N, CM, HM, S, TR>,
+//     // {
+//     //     XorRule::new(self, other)
+//     // }
+//     //
+//     // /// 取反规则
+//     // fn negation(self) -> NotRule<'a, N, CM, HM, S, TR, Self>
+//     // where
+//     //     Self: Sized,
+//     // {
+//     //     NotRule::new(self)
+//     // }
+// }
+
+/// 一条交易规则（Trading Rule）
+///
+/// 用于构建交易策略（Strategy），规则之间可以组合成更复杂的逻辑规则。
+pub trait Rule<'a> {
+    /// 数值类型（例如 DecimalNum、f64 等）
+    type Num: TrNum + 'static;
+    /// 买入成本模型
+    type CostBuy: CostModel<Self::Num> + Clone;
+    /// 卖出成本模型
+    type CostSell: CostModel<Self::Num> + Clone;
+    /// Bar 序列类型
+    type Series: BarSeries<'a, Self::Num> + 'a;
+    /// 交易记录类型
+    type TradingRec: TradingRecord<'a, Self::Num, Self::CostBuy, Self::CostSell, Self::Series>;
+
     /// 规则在给定索引下是否满足（不依赖交易记录）
     fn is_satisfied(&self, index: usize) -> bool {
         self.is_satisfied_with_record(index, None)
     }
 
     /// 规则在给定索引下是否满足（可选提供交易记录）
-    fn is_satisfied_with_record(&self, index: usize, trading_record: Option<&TR>) -> bool;
+    fn is_satisfied_with_record(
+        &self,
+        index: usize,
+        trading_record: Option<&Self::TradingRec>,
+    ) -> bool;
+
     //
     // /// 与另一条规则组合成 AND 规则
     // fn and<R>(self, other: R) -> AndRule<'a, N, CM, HM, S, TR, Self, R>
