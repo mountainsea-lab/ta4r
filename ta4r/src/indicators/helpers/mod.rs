11ff16eda6ff757c9e1afef256048a5f58f0e8d2
www1 <w1999wtw3537@sina.com>
2025-08-21 13:48:02 +0800

    LowPriceIndicator fixed

--- ta4r/src/indicators/helpers/mod.rs
+++ ta4r/src/indicators/helpers/mod.rs
@@ -30,7 +30,7 @@ pub mod constant_indicator;
 // pub mod gain_indicator;
 pub mod high_price_indicator;
 // pub mod highest_value_indicator;
-// pub mod low_price_indicator;
+pub mod low_price_indicator;
 // pub mod lowest_value_indicator;
 // pub mod open_price_indicator;
 // pub mod previous_value_indicator;
